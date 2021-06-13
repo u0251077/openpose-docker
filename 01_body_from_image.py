62dca300d491618448c86ae3043975e753d74429
c95lpy <u0251077@gmail.com>
2021-06-13 17:41:20 +0800

    intro how to fix code

--- 01_body_from_image.py
+++ 01_body_from_image.py
@@ -5,11 +5,14 @@ import cv2
 import os
 from sys import platform
 import argparse
+import pyopenpose as op
 
 try:
     # Import Openpose (Windows/Ubuntu/OSX)
     dir_path = os.path.dirname(os.path.realpath(__file__))
+    """
     try:
+        
         # Windows Import
         if platform == "win32":
             # Change these variables to point to the correct folder (Release/x64 etc.)
@@ -25,15 +28,15 @@ try:
     except ImportError as e:
         print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
         raise e
-
+    """
     # Flags
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_path", default="../../../examples/media/COCO_val2014_000000000192.jpg", help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
+    parser.add_argument("--image_path", default="../../examples/media/COCO_val2014_000000000192.jpg", help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
     args = parser.parse_known_args()
 
     # Custom Params (refer to include/openpose/flags.hpp for more parameters)
     params = dict()
-    params["model_folder"] = "../../../models/"
+    params["model_folder"] = "../../models/"
 
     # Add others in path?
     for i in range(0, len(args[1])):
